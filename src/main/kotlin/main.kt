1cfefe288ab577ec4370a9cbc84b262ebd82972a
Simon jigalin <simon.jigalin@gmail.com>
2023-11-09 14:59:38 +0100

    kamera geht zoomed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import org.bytedeco.opencv.global.opencv_imgproc.*
 import org.bytedeco.opencv.global.opencv_highgui.*
 import org.bytedeco.opencv.opencv_core.*
 import org.bytedeco.opencv.opencv_videoio.VideoCapture
@@ -15,13 +16,22 @@ fun main() {
 
     // Frame-Objekt für das Einlesen der Bilder
     val frame = Mat()
+    val zoomedFrame = Mat()
 
     while (true) {
         // Bild von der Kamera einlesen
         capture.read(frame)
         if (!frame.empty()) {
+            // Zoom anwenden
+            val center = Point(frame.cols() / 2, frame.rows() / 2)
+            val newSize = Size(frame.cols() / 2, frame.rows() / 2)
+            val roi = Rect(center.x() - newSize.width() / 2, center.y() - newSize.height() / 2, newSize.width(), newSize.height())
+
+            // Bild beschneiden und skalieren
+            resize(frame.apply(roi), zoomedFrame, frame.size())
+
             // Bild anzeigen
-            imshow("Webcam", frame)
+            imshow("Webcam", zoomedFrame)
         } else {
             println("Kein Frame erfasst.")
             break
