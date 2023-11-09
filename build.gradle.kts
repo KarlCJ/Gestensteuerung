658ba303678c551028c4c2739886d30780d0a07a
Simon jigalin <simon.jigalin@gmail.com>
2023-11-09 14:06:38 +0100

    kamera geht

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.10" // Verwenden Sie die aktuellste stabile Kotlin-Version
 }
 
-group = "me.reakt"
+group = "com.yourproject"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,13 +10,24 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+
+    // JavaCV (OpenCV für Java/Kotlin)
+    implementation("org.bytedeco:javacv:1.5.7") // Ersetzen Sie dies durch die neueste Version
+    implementation("org.bytedeco:javacv-platform:1.5.7") // Plattformübergreifende Unterstützung
+
+    // WebRTC-Abhängigkeit (abhängig von Ihrer spezifischen Implementierung oder Bibliothek)
+    // Beachten Sie, dass Sie die richtige Abhängigkeit für Ihre WebRTC-Implementierung finden müssen.
 }
 
-tasks.test {
-    useJUnit()
+tasks.compileKotlin {
+    kotlinOptions {
+        jvmTarget = "1.8" // oder '11' oder höher, je nach Ihrer JDK-Version
+    }
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+tasks.compileTestKotlin {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,39 @@
-fun main(){
-    println("small cock")
+import org.bytedeco.opencv.global.opencv_highgui.*
+import org.bytedeco.opencv.opencv_core.*
+import org.bytedeco.opencv.opencv_videoio.VideoCapture
+
+fun main() {
+    // Kamera initialisieren
+    val capture = VideoCapture(0)
+    if (!capture.isOpened) {
+        println("Kamera konnte nicht geöffnet werden.")
+        return
+    }
+
+    // Fenster für die Anzeige erstellen
+    namedWindow("Webcam")
+
+    // Frame-Objekt für das Einlesen der Bilder
+    val frame = Mat()
+
+    while (true) {
+        // Bild von der Kamera einlesen
+        capture.read(frame)
+        if (!frame.empty()) {
+            // Bild anzeigen
+            imshow("Webcam", frame)
+        } else {
+            println("Kein Frame erfasst.")
+            break
+        }
+
+        // Schleife beenden, wenn 'q' gedrückt wird
+        if (waitKey(30) == 'q'.toInt()) {
+            break
+        }
+    }
+
+    // Ressourcen freigeben
+    capture.release()
+    destroyAllWindows()
 }
