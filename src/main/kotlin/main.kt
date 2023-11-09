f1a2e7a55bf752c2bc034e5f6fb7b14032b61e32
kgoering <karl_reinhold.goering@smail.th-koeln.de>
2023-11-09 14:59:25 +0100

    GOGOGOGo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import org.bytedeco.opencv.global.opencv_imgproc.*
 import org.bytedeco.opencv.global.opencv_highgui.*
 import org.bytedeco.opencv.opencv_core.*
 import org.bytedeco.opencv.opencv_videoio.VideoCapture
@@ -15,20 +16,29 @@ fun main() {
 
     // Frame-Objekt für das Einlesen der Bilder
     val frame = Mat()
+    val zoomedFrame = Mat()
 
     while (true) {
         // Bild von der Kamera einlesen
         capture.read(frame)
         if (!frame.empty()) {
+            // Zoom anwenden
+            val center = Point(frame.cols() / 2, frame.rows() / 2)
+            val newSize = Size(frame.cols() / 2, frame.rows() / 2)
+            val roi = Rect(center.x() - newSize.width() / 2, center.y() - newSize.height() / 2, newSize.width(), newSize.height())
+
+            // Bild beschneiden und skalieren
+            resize(frame.apply(roi), zoomedFrame, frame.size())
+
             // Bild anzeigen
-            imshow("Webcam", frame)
+            imshow("Webcam", zoomedFrame)
         } else {
             println("Kein Frame erfasst.")
             break
         }
 
         // Schleife beenden, wenn 'q' gedrückt wird
-        if (waitKey(30) == 'q'.toInt()) {
+        if (waitKey(30) == 'q'.code) {
             break
         }
     }
