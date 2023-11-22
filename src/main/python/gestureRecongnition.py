7fa7400c003f148f3c5b4937f8dbbec67458b8d3
kgoering <karl_reinhold.goering@smail.th-koeln.de>
2023-11-22 17:14:59 +0100

    Handerkennung und Koordinaten des Zeigefingers funktionieren

--- src/main/python/gestureRecongnition.py
+++ src/main/python/gestureRecongnition.py
@@ -1,54 +1,50 @@
-#Import the necessary Packages for this software to run
+# Notwendige Bibliotheken importieren: sys für Systemfunktionen, mediapipe für Handgestenerkennung und cv2 für Bildverarbeitung
+import sys
 import mediapipe
 import cv2
 
-#Use MediaPipe to draw the hand framework over the top of hands it identifies in Real-Time
+# Mediapipe wird genutzt, um ein visuelles Modell der Hand zu erstellen
 drawingModule = mediapipe.solutions.drawing_utils
 handsModule = mediapipe.solutions.hands
 
-#Use CV2 Functionality to create a Video stream and add some values
+# Initialisierung der Kamera für Videoaufnahmen
 cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
-#Add confidence values and extra settings to MediaPipe hand tracking. As we are using a live video stream this is not a static
-#image mode, confidence values in regards to overall detection and tracking and we will only let two hands be tracked at the same time
-#More hands can be tracked at the same time if desired but will slow down the system
+# Einstellungen für die Handgestenerkennung: Modi, Vertrauenswerte und maximale Handanzahl
 with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7, max_num_hands=2) as hands:
 
-    #Create an infinite loop which will produce the live feed to our desktop and that will search for hands
+    # Endlosschleife für kontinuierliche Bildaufnahme und -verarbeitung
     while True:
         ret, frame = cap.read()
-        #Unedit the below line if your live feed is produced upsidedown
-        #flipped = cv2.flip(frame, flipCode = -1)
 
-        #Determines the frame size, 640 x 480 offers a nice balance between speed and accurate identification
+        # Anpassung der Bildgröße für optimale Verarbeitung
         frame1 = cv2.resize(frame, (640, 480))
 
-        #Produces the hand framework overlay ontop of the hand, you can choose the colour here too)
+        # Verarbeitung der Bildaufnahmen, um Handgesten zu erkennen
         results = hands.process(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB))
 
-        #In case the system sees multiple hands this if statment deals with that and produces another hand overlay
+        # Verarbeitung jeder erkannten Hand
         if results.multi_hand_landmarks != None:
             for handLandmarks in results.multi_hand_landmarks:
                 drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
 
-                #Below is Added Code to find and print to the shell the Location X-Y coordinates of Index Finger, Uncomment if desired
-                #for point in handsModule.HandLandmark:
+                # Ausgabe der Koordinaten des Zeigefingers
+                for point in handsModule.HandLandmark:
 
-                #normalizedLandmark = handLandmarks.landmark[point]
-                #pixelCoordinatesLandmark= drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, 640, 480)
+                    normalizedLandmark = handLandmarks.landmark[point]
+                    pixelCoordinatesLandmark = drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, 640, 480)
 
-                #Using the Finger Joint Identification Image we know that point 8 represents the tip of the Index Finger
-                #if point == 8:
-                #print(point)
-                #print(pixelCoordinatesLandmark)
-                #print(normalizedLandmark)
+                    if point == 8:  # Punkt 8 entspricht der Spitze des Zeigefingers
+                        print(point)
+                        print(pixelCoordinatesLandmark)
+                        print(normalizedLandmark)
+                        sys.stdout.flush()
 
-        #Below shows the current frame to the desktop
-        cv2.imshow("Frame", frame1);
+        # Anzeigen des verarbeiteten Bildes
+        cv2.imshow("Frame", frame1)
         key = cv2.waitKey(1) & 0xFF
 
-        #Below states that if the |q| is press on the keyboard it will stop the system
+        # Beenden des Programms bei Drücken der Taste 'q'
         if key == ord("q"):
             break
-
